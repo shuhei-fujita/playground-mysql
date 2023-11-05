bcc97179c1d42a110b0103bfd5f4194c2f95da81
shuhei-fujita <syuheifujita0117@gmail.com>
2023-11-05 21:57:24 +0900

    feat: add auth
    closes #3

---
+++ api/src/auth.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, HTTPException
+from passlib.context import CryptContext
+
+router = APIRouter()
+
+class User(BaseModel):
+    username: str
+    password: str
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+@router.post("/signup")
+async def sign_up(user: User):
+    # ユーザー登録のロジックを実装します
+    hashed_password = pwd_context.hash(user.password)
+    return {"message": "ユーザーが作成されました"}
+
+@router.post("/signin")
+async def sign_in(user: User):
+    # ログイン処理とJWTトークンの発行を実装します
+    return {"message": "ログインしました"}
